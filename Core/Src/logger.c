3e0154e991049f72e76cb5b45266f01e037606d6
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-05-04 08:48:07 +0200

    logMessage tested OK; mutex to be added

--- Core/Src/logger.c
+++ Core/Src/logger.c
@@ -51,19 +51,23 @@ void logMessage(enum LogLevel_t level, const char* msg, ...)
             break;                        
         }
         
+        /* place level severity text */
         strcpy(printBuf, pText);
         size_t len = strlen(pText);
         printBuf[len++] = ':';
-
+        /* terminating string */
         pText = "\n\r";
-
+        /* process variadic arguments */
         va_list args;
         va_start(args, msg);
-        len = len + vsnprintf(printBuf + len, /*PRINT_BUF_LENGTH*/20 - len - strlen(pText), msg, args);
+        vsnprintf(printBuf + len, PRINT_BUF_LENGTH - len - strlen(pText), msg, args);
         va_end(args);
-
+        /* get the actual current string length */
+        len = strlen(printBuf);
+        /* add the terminating string */
         strcpy(printBuf + len, pText);
         len += strlen(pText);
+        /* transmit the string over USB link */
         CDC_Transmit_FS((uint8_t*)printBuf, len);
         HAL_GPIO_WritePin(TEST4_GPIO_Port, TEST4_Pin, GPIO_PIN_RESET);
     }
